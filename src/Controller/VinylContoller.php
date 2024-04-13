15c39882527de2086b9538831ba5b8b54f1f8193
Mikhail Fedosov <mikhail.fedosov@vk.team>
2024-04-13 16:19:24 +0300

    Cool looking pages HTML+CSS+Twig magic

--- src/Controller/VinylContoller.php
+++ src/Controller/VinylContoller.php
@@ -9,7 +9,7 @@ use function Symfony\Component\String\u;
 
 class VinylContoller extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
         $params = [];
@@ -17,14 +17,23 @@ class VinylContoller extends AbstractController
         $params['tracks'] = [
             ['by' => 'Bon Jovi', 'name' => 'It\'s my life'],
             ['by' => 'Kanye West', 'name' => 'Stronger'],
+            ['by' => 'Gangsta\'s Paradise', 'name' => 'Coolio'],
+            ['by' => 'Waterfalls', 'name' => 'TLC'],
+            ['by' => 'Creep', 'name' => 'TLC'],
+            ['by' => 'Kiss from a Rose', 'name' => 'Seal'],
+            ['by' => 'On Bended Knee', 'name' => 'Boyz II Men'],
+            ['by' => 'Fantasy', 'name' => 'Mariah Carey'],
+            ['by' => 'Take a Bow', 'name' => 'Madonna'],
+            ['by' => 'Another Night', 'name' => 'Real McCoy'],
         ];
         return $this->render('vinyl/homepage.html.twig', $params);
     }
 
-    #[Route('/browse/{genre}')]
-    public function browser(?string $genre = null): Response
+    #[Route('/browse/{genre}', name: 'app_browse')]
+    public function browse(?string $genre = null): Response
     {
-        $genre ? $title = 'Genre: ' . u(str_replace('-', ' ', $genre))->title(true) : $title = 'All Genres';
-        return new Response($title);
+        $genre ? $title = u(str_replace('-', ' ', $genre)) : $title = null;
+        $params = ['title' => $title];
+        return $this->render('vinyl/browse.html.twig', $params);
     }
 }
