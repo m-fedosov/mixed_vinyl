b6e0829f287ed91972232713cea97e599c2806fc
Mikhail Fedosov <mikhail.fedosov@vk.team>
2024-04-14 20:12:23 +0300

    Some controller changes

---
+++ src/Controller/SongController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong($id, LoggerInterface $logger): Response
+    {
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+        $logger->info("hi sweety");
+        return $this->json($song);
+    }
+}
